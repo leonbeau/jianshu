0ac9796bed0afe2391f35e9b1f7a1bbebe36ae55
leon <leon@hooc.top>
2019-08-04 11:23:49 +0800

    学习header布局

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.js';
+import Header from './common/header/index'
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/common/header/index.js
@@ -0,0 +1,45 @@
+import React,{ Component } from 'react';
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button,
+    SearchWrapper,
+
+} from './style';
+import iconfont from '../../static/iconfont/iconfont.css';
+class Header extends Component{
+    render(){
+        return(
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登录</NavItem>
+                    <NavItem className="right">
+                     <span className="iconfont">&#xe636;</span>
+                    </NavItem>
+                    <SearchWrapper>
+                        <NavSearch></NavSearch>
+                        <span className="iconfont">&#xe62a;</span>
+                    </SearchWrapper>
+                   
+                </Nav>
+                <Addition>
+                    <Button className="writting">
+                        <span className="iconfont">&#xe615; </span>
+                        写文章
+                    </Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
+    
+    }
+}
+
+export default Header;
