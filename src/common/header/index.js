4a04794f2f812543fe5ad6ac26bf8a8ed514c4b9
leon <leon@hooc.top>
2019-08-05 16:12:24 +0800

    管理状态

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import * as actionCreators from '../../store/actionCreators';
 import {
     HeaderWrapper,
     Logo,
@@ -60,16 +61,10 @@ const mapStateToProps = (state) => {
 const mapDispathToProps = (dispatch)=>{
     return{
        handleInputFocus(){
-          const action = {
-              type:'search_focus'
-          };
-          dispatch(action);
+          dispatch(actionCreators.searchFocus());
        },
        handleInputBlur(){
-            const action={
-                type:'search_blur'
-            }
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
        }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import {createStore} from 'redux';
-import reducer from './reducer'
-const store = createStore(reducer);
+import reducer from './reducer';
+import { compose } from 'redux';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,composeEnhancers());
 
 export default store;
